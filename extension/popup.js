864b7e8629d7a18e05c9b638bb5a35dc1269d00f
buggyj <buggyjef@gmail.com>
2017-11-03 17:48:55 +0100

    fixes #9 add toolbar popup

---
+++ extension/popup.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('#config').addEventListener('click', function() {
+    window.open(chrome.runtime.getURL("options.html"));
+  });
+  
+  document.querySelector('#about').addEventListener('click', function() {
+    window.open("https://github.com/buggyj/bsaver");
+  });
+
+});
